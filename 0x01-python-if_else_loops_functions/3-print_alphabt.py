8c3aec21e4a0ed110ef9f318c7602bd007c88eca
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-06 22:46:55 +0300

    Remove increment in loop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    print("{0:c}".format(0 if c == 113 or c == 101 else c), end='')
-    c += 1
+    if c == 113 or c == 101:
+        continue
+    print("{0:c}".format(c), end='')
