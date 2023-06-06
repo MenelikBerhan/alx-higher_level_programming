97c05068d033def72dd0b3d632d3114f653e9165
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-06 21:45:24 +0300

    Use .format() to fix requirement error

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 for c in range(97, 123):
-    if c == 122:
-        end = '\n'
-    else:
-        end = ''
-    print(f"{c:c}", end=end)
+    print("{0:c}".format(c), end='' if c < 122 else '\n')
     c += 1
