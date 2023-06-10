e3fc1ebb7e9983a719b965a861e101a040368d96
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-10 21:16:15 +0300

    Use a helper function to index list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 #include <stdio.h>
+
+
+/**
+ * get_element - finds the element at index index in a linked list list
+ * @head: a linked list
+ * @index: index of node to be found
+ *
+ * Return: a pointer to the node at index
+*/
+listint_t *get_element(listint_t *head, int index)
+{
+	int i;
+
+	for (i = 0; i < index; i++)
+		head = head->next;
+	return (head);
+}
+
 /**
  * is_palindrome - checks if a linked list pointed by head is a plindrome
  * @head: a pointer to a linked list
@@ -9,7 +27,7 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp;
-	int i, len = 0, list[1024];
+	int i, len = 0;
 
 	if (!head || !(*head))
 		return (1);
@@ -20,11 +38,8 @@ int is_palindrome(listint_t **head)
 		len++;
 	}
 
-	for (i = 0, temp = *head; i < len; i++, temp = temp->next)
-		list[i] = temp->n;
-
-	for (i = 0; i < len / 2; i++)
-		if (list[i] != list[len - 1 - i])
+	for (i = 0, temp = *head; i < len / 2; i++, temp = temp->next)
+		if (temp->n != (get_element(*head, len - 1 - i))->n)
 			return (0);
 
 	return (1);
