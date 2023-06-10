14615103f2e36f1b3ce17a2ebbfda97705828814
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-10 20:41:36 +0300

    Add newline printing for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,7 @@ def print_reversed_list_integer(my_list=[]):
     Args:
         my_list: a list
     '''
+    if len(my_list) == 0:
+        print()
     for i in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[i]))
