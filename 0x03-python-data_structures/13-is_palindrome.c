9701f6bd0c3d781e1dc8d1f121433e426436f86a
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-10 22:00:13 +0300

    Use a helper function

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,28 @@
 #include "lists.h"
 #include <stdio.h>
+
+/**
+ * is_palindrome_helper - checks the n field value of the first
+ *		and last node of a linked list
+ * @head: a linked list
+ * @len: length of linked list
+ *
+ * Return: 1 if first and last node n values are equal, or 0 if not
+*/
+int is_palindrome_helper(listint_t *head, int len)
+{
+	listint_t *temp;
+	int i;
+
+	if (!head || !head->next || len == 1 || len == 0)
+		return (1);
+	temp = head;
+	for (i = 1; i < len; i++)
+		temp = temp->next;
+	if (head->n != temp->n)
+		return (0);
+	return (is_palindrome_helper(head->next, len - 2));
+}
 /**
  * is_palindrome - checks if a linked list pointed by head is a plindrome
  * @head: a pointer to a linked list
@@ -9,7 +32,7 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp;
-	int i, len = 0, *list;
+	int len = 0;
 
 	if (!head || !(*head) || !(*head)->next)
 		return (1);
@@ -19,24 +42,6 @@ int is_palindrome(listint_t **head)
 		temp = temp->next;
 		len++;
 	}
-	list = malloc(sizeof(int) * len);
-	if (!list)
-	{
-		perror("malloc failure");
-		exit(98);
-	}
-	for (i = 0, temp = *head; i < len; i++, temp = temp->next)
-		list[i] = temp->n;
 
-	for (i = 0; i < len / 2; i++)
-	{
-		if (list[i] != list[len - 1 - i])
-		{
-			free(list);
-			return (0);
-		}
-	}
-	free(list);
-
-	return (1);
+	return (is_palindrome_helper(*head, len));
 }
