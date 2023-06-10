b62a40a462c46f954bb67f1f495dc684e0d6fe86
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-10 20:35:30 +0300

    Remove printing empty line for empty list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,9 +6,9 @@ def print_matrix_integer(matrix=[[]]):
         matrix: a matrix of integers
     '''
     for row in matrix:
-        if len(row) == 0:
-            print()
-            continue
+        # if len(row) == 0:
+        #     print()
+        #     continue
         for i in range(len(row)):
             print("{:d}".format(row[i]),
                   end=', ' if i != len(row) - 1 else '\n')
