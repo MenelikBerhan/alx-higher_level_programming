921ec434cbda31b886db94cc9a24d9c252eecd82
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-10 20:50:55 +0300

    Use an array instead of malloc'ed pointer

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,7 +9,7 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp;
-	int i, len = 0, *list;
+	int i, len = 0, list[1024];
 
 	if (!head || !(*head))
 		return (1);
@@ -19,24 +19,13 @@ int is_palindrome(listint_t **head)
 		temp = temp->next;
 		len++;
 	}
-	list = malloc(sizeof(int) * len);
-	if (!list)
-	{
-		perror("malloc failure");
-		exit(98);
-	}
+
 	for (i = 0, temp = *head; i < len; i++, temp = temp->next)
 		list[i] = temp->n;
 
 	for (i = 0; i < len / 2; i++)
-	{
 		if (list[i] != list[len - 1 - i])
-		{
-			free(list);
 			return (0);
-		}
-	}
-	free(list);
 
 	return (1);
 }
