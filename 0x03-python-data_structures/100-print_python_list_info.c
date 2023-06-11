e7e620d4135beb4b75f7d2b833fe73e64fcd1072
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-11 03:26:35 +0300

    Add file for Task 14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints information about a Python list
+ * @p: a PyObject pointer to the list
+ *
+*/
+void print_python_list_info(PyObject *p)
+{
+	int i, len;
+	static int alloc_ed = -1;
+	static PyObject *temp;
+
+	len = PyList_Size(p);
+	if (alloc_ed == -1 || p != temp)
+		alloc_ed = len;
+	else if (len > alloc_ed)
+		alloc_ed  = len + (len >> 3) + (len < 9 ? 3 : 6);
+
+	temp = p;
+	printf("[*] Size of the Python List = %d\n", len);
+	printf("[*] Allocated = %d\n", alloc_ed);
+
+	for (i = 0; i < len; i++)
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+
+}
