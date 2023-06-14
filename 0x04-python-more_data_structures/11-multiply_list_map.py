9876499a6de4bfaf1273ae44e23f2f3a83a2d703
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-15 02:33:03 +0300

    Remove function description

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    '''Creates a new list with all values of a list multiplied by number
-
-    Args:
-        my_list: a list used to create the new list
-        number: integer to multiply values of my_list
-
-    Returns: the newly created list
-    '''
     return (list(map(lambda x: 2 * x, my_list)))
