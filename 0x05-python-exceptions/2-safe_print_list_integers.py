30c2b117b4c0b75c8c00445ef36edab2afe553d9
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-27 00:15:47 +0300

    Use a while loop and add if not my_list or x == 0

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,13 +10,14 @@ def safe_print_list_integers(my_list=[], x=0):
     if not my_list or x == 0:
         print("")
         return 0
-    try:
-        for i in range(0, x):
+    i, j = 0, 0
+    while i < x:
+        try:
             print("{:d}".format(my_list[i]), end="\n" if i == x - 1 else "")
-        return i + 1
-    except (ValueError, TypeError) as ex:
-        if (i < x - 1):
-            return (i + safe_print_list_integers(my_list[i + 1:], x - i - 1))
-        else:
-            print("")
-            return i
+            i += 1
+        except (ValueError, TypeError):
+            if i == x - 1:
+                print("")
+            j += 1
+            i += 1
+    return i - j
