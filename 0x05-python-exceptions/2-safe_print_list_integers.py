9897ffc51f7ef0c247576a03d4d24bb16d011ac0
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-27 00:06:20 +0300

    Use recursion to test if it works

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,14 +10,13 @@ def safe_print_list_integers(my_list=[], x=0):
     if not my_list or x == 0:
         print("")
         return 0
-    i, j = 0, 0
-    while i < x:
-        try:
+    try:
+        for i in range(0, x):
             print("{:d}".format(my_list[i]), end="\n" if i == x - 1 else "")
-            i += 1
-        except (ValueError, TypeError):
-            if i == x - 1:
-                print("")
-            j += 1
-            i += 1
-    return i - j
+        return i + 1
+    except (ValueError, TypeError) as ex:
+        if (i < x - 1):
+            return (i + safe_print_list_integers(my_list[i + 1:], x - i - 1))
+        else:
+            print("")
+            return i
