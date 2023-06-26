e8e7926e53509d22ba1d286017aada3fe141b2db
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-26 23:58:28 +0300

    Use while loop instead of recursion

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,13 +7,16 @@ def safe_print_list_integers(my_list=[], x=0):
         x: no. of elements to access in my_list list
     Returns: the actually printed number of integer elements
     '''
-    try:
-        for i in range(0, x):
+    # if not my_list:
+    #     return 0
+    i, j = 0, 0
+    while i < x:
+        try:
             print("{:d}".format(my_list[i]), end="\n" if i == x - 1 else "")
-        return i + 1
-    except (ValueError, TypeError) as ex:
-        if (i < x - 1):
-            return (i + safe_print_list_integers(my_list[i + 1:], x - i - 1))
-        else:
-            print("")
-            return i
+            i += 1
+        except (ValueError, TypeError):
+            if i == x - 1:
+                print("")
+            j += 1
+            i += 1
+    return i - j
