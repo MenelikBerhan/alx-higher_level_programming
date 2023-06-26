51476b8ee51ded19c96d15fc713c319ea324d910
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-27 00:12:21 +0300

    Add check if !my_list  or x == 0 and print '\n'

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,9 +7,10 @@ def safe_print_list(my_list=[], x=0):
         x: no.of elements to print
     Returns: actually printed number of elements
     '''
+    if not my_list or x == 0:
+        print("")
+        return 0
     try:
-        if not my_list:
-            return 0
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
         print("")
