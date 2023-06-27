061f0f089d84ed4681cb337147016b1a08eabf5a
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-28 02:20:49 +0300

    Revert changes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -81,13 +81,11 @@ class Square():
                 ##
                 ##
         """
+        if self.__size == 0:
+            print()
+            return
         for y in range(0, self.__position[1]):
             print()
-        if self.__size == 0:
-            if self.__position[1] == 0:
-                print()
-            return
-
         for row in range(0, self.__size):
             for x in range(0, self.__position[0]):
                 print(" ", end="")
