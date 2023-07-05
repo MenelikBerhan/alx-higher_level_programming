15ef8ac94f999de7e787c87f26494a38918e3d81
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-07-05 03:33:37 +0300

    Add docstring and change excp message

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""
+Module containing LockedClass
+"""
+
 
 class LockedClass():
     """Class that doesn't allow the user to set
     a new instance attribute except `first_name`"""
 
     def __setattr__(self, attribute, value):
+        """sets attribute only for `first_name`"""
         if attribute != "first_name":
-            raise AttributeError("Can't set {} attribute".format(attribute))
+            raise AttributeError("'LockedClass' object has no attribute '{}'"
+                                 .format(attribute))
         else:
             self.__dict__[attribute] = value
