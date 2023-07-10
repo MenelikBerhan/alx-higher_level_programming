03afc3f88e470866cbf98fa053e0a39d4be33fb2
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-07-11 02:32:03 +0300

    Add files for Task 5 & 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module containing `BaseGeometry` class"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry that defines a basic gemoetric shape"""
+
+    def area(self):
+        """Raises an `Exception`"""
+        raise Exception("area() is not implemented")
