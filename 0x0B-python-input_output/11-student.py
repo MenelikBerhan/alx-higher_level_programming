fd8f62e9d64570d67704a54a4f19d50f1125ed02
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-07-12 02:42:15 +0300

    Add file for Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module containing `Student` class"""
+
+
+class Student:
+    """Defines a student by `first_name`, `last_name` and `age`"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a `Student` instance or object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """`dict`: Returns a dictionay representation of a `Student` instance.
+        If `attrs` is a list of strings, only attribute names contained in
+        this list will be retrieved. Otherwise, all attrs will be retrieved"""
+        my_dict = {}
+        if type(attrs) == list and all([type(x) == str for x in attrs]):
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    my_dict[key] = value
+            return my_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Updates the instance attribute values from a dictionary"""
+        for key, value in json.items():
+            self.__dict__[key] = value
