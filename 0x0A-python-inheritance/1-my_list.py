0683eab57c1becc3cddee0be5b5437eb8eb03c48
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-07-11 04:39:41 +0300

    Remove __init__

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,9 @@
 class MyList(list):
     """A class tha inherits the `list` class"""
 
-    def __init__(self, *args):
-        """Initalizes a MyList instance object"""
-        super().__init__(*args)
+    # def __init__(self, *args):
+    #     """Initalizes a MyList instance object"""
+    #     super().__init__(*args)
 
     def print_sorted(self):
         """Prints a MyList list sorted in ascending order"""
