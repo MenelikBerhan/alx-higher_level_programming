5faacf329b8c9e7c9605b1f9fdb8e7fb103b5454
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-07-11 03:19:36 +0300

    Add superclass init

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-Module containing `MyList` class
-"""
+"""Module containing `MyList` class"""
 
 
 class MyList(list):
-    """
-    A class tha inherits the `list` class
-    """
+    """A class tha inherits the `list` class"""
+
+    def __init__(self, *args):
+        """Initalizes a MyList instance object"""
+        super().__init__(*args)
+
     def print_sorted(self):
-        """
-        Prints a MyList list sorted in ascending order
-        """
+        """Prints a MyList list sorted in ascending order"""
         print(sorted(self))
