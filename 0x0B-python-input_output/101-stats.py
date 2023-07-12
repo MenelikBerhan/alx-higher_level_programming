076412da865cf36cec0e29edfe03feb7b2b925c4
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-07-12 05:44:00 +0300

    Add try/except for reading status and size

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,25 +4,42 @@ and computes metrics"""
 import sys
 
 
+count = 0
+total_size = 0
+status_dict = {}
+stat_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 try:
-    count = 0
-    total_size = 0
-    status_dict = {}
+
     for line in sys.stdin:
         if count == 10:
             print("File size: {}".format(total_size))
-            for k in sorted(status_dict.keys()):
+            for k in sorted(status_dict):
                 print("{}: {}".format(k, status_dict[k]))
-            count = 0
+            count = 1
+        else:
+            count += 1
         row = line.split()
-        total_size += int(row[8])
-        status = row[7]
-        status_dict[status] = status_dict.get(status, 0) + 1
-        count += 1
+        try:
+            total_size += int(row[-1])
+        except (IndexError, ValueError):
+            pass
+        # total_size += int(row[8])
+        # status = row[7]
+        # status_dict[status] = status_dict.get(status, 0) + 1
+        try:
+            if row[-2] in stat_codes:
+                if status_dict.get(row[-2], -1) == -1:
+                    status_dict[row[-2]] = 1
+                else:
+                    status_dict[row[-2]] += 1
+        except IndexError:
+            pass
+
+    print("File size: {}".format(total_size))
+    for k in sorted(status_dict):
+        print("{}: {}".format(k, status_dict[k]))
 except KeyboardInterrupt:
-    out = "File size: {}\n".format(total_size)
-    status = ""
-    for k in sorted(status_dict.keys()):
-        status = status + "{}: {}\n".format(k, status_dict[k])
-    print(out + status, end="")
+    print("File size: {}".format(total_size))
+    for k in sorted(status_dict):
+        print("{}: {}".format(k, status_dict[k]))
     raise
