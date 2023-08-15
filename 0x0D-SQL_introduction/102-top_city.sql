29982101e46d1a66229aa02d17c3b6660f0a7526
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-08-16 02:35:40 +0300

    Use avg_temp instead of avg

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 SELECT city, SUM(value)/COUNT(city) as avg_temp FROM temperatures
 WHERE month = '7' OR month = '8'
-GROUP BY city ORDER BY avg DESC
+GROUP BY city ORDER BY avg_temp DESC
 LIMIT 3;
