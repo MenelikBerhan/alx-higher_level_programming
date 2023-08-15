25b39300352a1b2c16d10adb37b50af8a73d0879
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-08-16 02:32:39 +0300

    Add file for Task 19

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT city, sum(value)/count(city) as avg FROM temperatures
+WHERE month = '7' OR month = '8'
+GROUP BY city ORDER BY avg DESC
+LIMIT 3;
