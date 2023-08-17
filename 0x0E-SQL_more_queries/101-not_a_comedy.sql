04c6206e4800c2c22a077c30111f2043572d3292
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-08-17 05:23:32 +0300

    Use NOT IN instead of EXCEPT

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -9,13 +9,20 @@
 --         ON s.id = tv.show_id
 -- WHERE tv.genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
 -- ORDER BY title;
+
 SELECT DISTINCT s.title
 FROM tv_shows s
     LEFT JOIN tv_show_genres tv
-    ON s.id = tv.show_id
+        ON s.id = tv.show_id
     LEFT JOIN tv_genres g
-    ON tv.genre_id = g.id
-
-    WHERE tv.show_id NOT IN (SELECT t.show_id FROM tv_show_genres t WHERE t.genre_id = 
-    (SELECT tv_genres.id FROM tv_genres WHERE tv_genres.name = "Comedy"))
-    ;
+        ON tv.genre_id = g.id
+WHERE s.title NOT IN (
+    SELECT s.title
+    FROM tv_genres g
+    INNER JOIN tv_show_genres tv
+        ON g.id = tv.genre_id
+    INNER JOIN tv_shows s
+        ON tv.show_id = s.id
+    WHERE g.name = 'Comedy'
+)
+ORDER BY s.title;
