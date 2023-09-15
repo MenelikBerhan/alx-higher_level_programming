e97b04eba937d144fa6b2bee0d78cdc87630c9d9
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-15 20:43:17 +0300

    Add try-except

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,12 +5,15 @@ starting with `N` (upper N) from the database `hbtn_0e_0_usa`"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    no_rows = cur.execute("""SELECT * FROM states
-                          WHERE name LIKE 'N%' ORDER BY id""")
-    rows = cur.fetchall()
+    try:
+        db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cur = db.cursor()
+        no_rows = cur.execute("""SELECT * FROM states
+                            WHERE name LIKE 'N%' ORDER BY id""")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        print("MySQL Error: %s" % str(e))
     for row in rows:
         print(row)
     cur.close()
