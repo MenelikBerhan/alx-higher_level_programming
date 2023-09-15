0893b78ce2639211fad50fd593e5a81e10419bb9
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-16 01:47:46 +0300

    Add files for Task 7 & 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a script that prints the first `State` object
+from the database `hbtn_0e_6_usa`"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], quote_plus(sys.argv[2]), sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State.id, State.name).order_by(State.id).first()
+    if not state:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """Contains the class definition of a State and
 an instance Base = declarative_base()"""
-
-# from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-# engine = create_engine('mysql+mysqldb://root:Mysql%40alx1@localhost
-# :3306/hbtn_0e_6_usa')
+
 Base = declarative_base()
 
 
