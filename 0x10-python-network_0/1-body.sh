1ecdd82487d08222287c2bcdb4add96a895164f9
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-30 21:39:54 +0300

    Use -f flag to display only body with 200 status

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sL -X GET "$1"
+# takes in a URL, sends a GET request to the URL, and displays the body of the response
+#+ if it's status code is 200(success)
+curl -sfL -X GET "$1"
