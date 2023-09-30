d60f6185cddfd66fda08cbd6c0953f645caa479c
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-01 01:22:29 +0300

    To test checker

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON (from a file passed as second argument) POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -sX POST -d @"$2" "$1"
