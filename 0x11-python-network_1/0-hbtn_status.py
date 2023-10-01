914603722c4999df042219d661e7abdacda9f4d0
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-01 14:53:59 +0300

    Fix code being  executed when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status using urlib"""
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", body.decode("utf-8"))
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,15 +2,15 @@
 """takes in a URL and an email, sends a POST request to
 the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)"""
-import sys
-import urllib.request
-import urllib.parse
+if __name__ == "__main__":
+    import sys
+    import urllib.request
 
-url = sys.argv[1]
-values = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    body = response.read()
-    print(body.decode('utf-8'))
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
