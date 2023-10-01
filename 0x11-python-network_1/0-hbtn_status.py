e76b84a2a12d56eea4b63c5aeb9daadee00bb307
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-01 14:12:30 +0300

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status using urlib"""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf-8"))
