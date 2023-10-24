684acd8f78ddb64734fbdaa3ab0a06ad6ec6486f
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-25 02:40:37 +0300

    Add comment

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file.
+/*
+Script that reads and prints the content of a file.
+  The first argument is the file path
+  The content of the file will be read in utf-8
+  If an error occurred during the reading, prints the error object
+*/
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8',
